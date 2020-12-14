182c917c9880f15c673c5c7ab253251e3078c4dd
Mario Sever <34382956+mariosever@users.noreply.github.com>
2020-12-14 10:55:44 +0100

    Add files via upload

---
+++ npzw_tema/front-page.php
@@ -0,0 +1,16 @@
+<?php /* front-page.php */ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Npzw wordpress stranica</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<h1>Npzw custom wordpress stranica</h1>
+
+	<p>Jeee, radi!!</p>
+
+</body>
+</html>
